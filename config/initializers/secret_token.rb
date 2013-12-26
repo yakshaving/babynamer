0bec5f85a593102dd69e836ee4c6d2bc11b36a60
ash bhoopathy <ash.bhoopathy@gmail.com>
2013-12-25 19:32:10 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Babynames::Application.config.secret_key_base = 'b910c8be3e91a7dfa90401a04d7d184b741aae7fec4259a1e25cc351def8148564a52bd899471ed69c6158ed3144cfde86eeaeecba8e12737e8eb408be62e7e3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Babynames::Application.config.session_store :cookie_store, key: '_babynames_session'
